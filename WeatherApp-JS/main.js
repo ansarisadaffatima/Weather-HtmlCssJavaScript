2798eff1a69aa9c8ec561071c5c32ebaff891f41
Sadaf Fatima Ansari <ansarisadaffatima@gmail.com>
2022-01-11 12:54:19 +0530

    Weather Project using Html,Css and JavaScript

---
+++ WeatherApp-JS/main.js
@@ -0,0 +1,61 @@
+const api = {
+    key: "f5aad37c01115f854bb3baba0c99a2d6",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+  
+  function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+  }
+  
+  function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  }
+  
+  function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+  
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp).toFixed(0)}<span>°c</span>`;
+  
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+  
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+
+    let bg;
+    let evalTemp = `${Math.round(weather.main.temp).toFixed(0)}`;
+    if(evalTemp<0){
+      bg = "url('img/Snow.jpg')";
+    }else if(evalTemp > 22){
+      bg = "url('img/Summer.jpg')";
+    }else{
+      bg = "url('img/Winter.jpg')";
+    }
+    document.body.style.backgroundImage = bg;
+  }
+  
+  function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`;
+  }
